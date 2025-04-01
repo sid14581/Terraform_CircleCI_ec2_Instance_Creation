bb46a4d37d9ffa8f92b1abcde64e4efba7a0f30d
sid14581 <siddhardh.kbv@gmail.com>
2025-04-02 02:48:48 +0530

    creating basic instance

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "devopsbyexample-tf-state-one"
+    key    = "platform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-04f167a56786e4b09"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0089c47b93e3401ca"
+}
