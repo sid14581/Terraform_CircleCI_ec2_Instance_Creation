f2d49fddc94d3042f0dc0533a6cff196588f8b3e
sid14581 <siddhardh.kbv@gmail.com>
2025-04-02 14:52:15 +0530

    changed the subnet ID

--- main.tf
+++ main.tf
@@ -19,5 +19,5 @@ terraform {
 resource "aws_instance" "ubuntu" {
   ami           = "ami-04f167a56786e4b09"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0089c47b93e3401ca"
+  subnet_id     = "subnet-0e736c939b076517a"
 }
