933505310bac5e4c11e438af9fa4a44a392c603d
sid14581 <siddhardh.kbv@gmail.com>
2025-05-27 17:39:28 +0530

    creating ec2 instance

--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ terraform {
 }
 
 
-# resource "aws_instance" "ubuntu" {
-#   ami           = "ami-04f167a56786e4b09"
-#   instance_type = "t2.micro"
-#   subnet_id     = "subnet-0e736c939b076517a"
-# }
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-04f167a56786e4b09"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0e736c939b076517a"
+}
 
 # Remove the comment to create the cluster
